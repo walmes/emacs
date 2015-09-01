ccf40d1a5b3f31722adbe437c6b70c7765860d1b
Walmes Zeviani <walmeszeviani@gmail.com>
2015-09-01 19:53:39 -0300

    Menu of options for install or copy/transfer of files and remove ProgressBar function.

--- dotemacs.sh
+++ dotemacs.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
 #----------------------------------------------------------------------
-# From: http://stackoverflow.com/questions/238073/how-to-add-a-progress-bar-to-a-shell-script
-
-# ProgressBar function.
-# Input is currentState($1) and totalState($2).
-function ProgressBar {
-    # Process data.
-    let _progress=(${1}*100/${2}*100)/100
-    let _done=(${_progress}*4)/10
-    let _left=40-$_done
-    # Build progressbar string lengths.
-    _fill=$(printf "%${_done}s")
-    _empty=$(printf "%${_left}s")
-    # Build progressbar strings and print the ProgressBar line
-    # Output example:
-    #   Progress : [########################################] 100%
-    printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%\n"
-}
-
-# Total number of steps.
-_end=7
-step=0
-
-##----------------------------------------------------------------------
-## Welcome message.
+# Welcome message.
 
 cat << EOF
 --------------------------------------------------------------------------
@@ -36,22 +13,22 @@ cat << EOF
 --------------------------------------------------------------------------
 EOF
 
-read -s opcao; echo; echo
+#----------------------------------------------------------------------
+# Install Emacs-24.5.
 
-##----------------------------------------------------------------------
-## Install Emacs-24.5.
+function installemacs24.5 {
 
-if which emacs-24.5 >/dev/null
-then
+    if which emacs-24.5 >/dev/null
+    then
+        MSG="Emacs-24.5 is installed. Do you want reinstall it? [y]es/[n]o"
+        echo; echo
+    else
+        MSG="Emacs-24.5 isn't installed. Do you want install it? [y]es/[n]o"
+        echo; echo
+    fi
+
     echo ------------------------------------------------------------
-    echo "Emacs-24.5 is installed."
-    which emacs-24.5
-    ProgressBar ${step} ${_end}; step=$((step+1))
-    echo; echo
-else
-    echo ------------------------------------------------------------
-    echo "Emacs-24.5 isn't installed. Do you want to install it? [ y ]"
-    ProgressBar ${step} ${_end}; step=$((step+1))
+    echo "$MSG"
     read opcao
     case $opcao in
         y )
@@ -85,38 +62,34 @@ else
             echo "Skipped."; echo; echo
             ;;
     esac 
-fi
+    
+    echo ------------------------------------------------------------
+    echo "Install emacs-goodies-el? [y]es/[n]o"
+    read opcao
+    case $opcao in
+        y )
+            echo "Install Emacs' friends."
+            sudo apt-get install emacs-goodies-el -y
+            echo; echo
+            ;;
+        * )
+            echo "Skipped."; echo; echo
+            ;;
+    esac 
+}
 
-##----------------------------------------------------------------------
-## Install Emacs' friends.
+#----------------------------------------------------------------------
+# Make desktop application for Emacs-24.5.
 
-echo ------------------------------------------------------------
-echo "Install emacs-goodies-el? [ y ]"
-ProgressBar ${step} ${_end}; step=$((step+1))
-read opcao
-case $opcao in
-    y )
-        echo "Install Emacs' friends."
-        sudo apt-get install emacs-goodies-el -y
-        echo; echo
-        ;;
-    * )
-        echo "Skipped."; echo; echo
-        ;;
-esac 
-
-##----------------------------------------------------------------------
-## Make desktop application for Emacs-24.5.
-
-echo ------------------------------------------------------------
-echo "Make desktop application for Emacs-24.5.? [ y ]"
-ProgressBar ${step} ${_end}; step=$((step+1))
-read opcao
-case $opcao in
-    y )
-        echo "Creating \"/usr/share/applications/emacs-24.5.desktop\"."
-        echo; echo
-        cat > emacs-24.5.desktop << EOL
+function makedesktopapp {
+    echo ------------------------------------------------------------
+    echo "Make desktop application for Emacs-24.5.? [y]es/[q]uit"
+    read opcao
+    case $opcao in
+        y )
+            echo "Creating \"/usr/share/applications/emacs-24.5.desktop\"."
+            echo; echo
+            cat > emacs-24.5.desktop << EOL
 [Desktop Entry]
 Version=1.0
 Name=GNU Emacs 24.5
@@ -131,115 +104,123 @@ Terminal=false
 Categories=Utility;Development;TextEditor;
 StartupWMClass=emacs-24.5
 EOL
-sudo mv emacs-24.5.desktop /usr/share/applications/
-        ;;
-    * )
-        echo "Skipped."; echo; echo
-        ;;
-esac 
-
-##----------------------------------------------------------------------
-
-emacsddir="$HOME/.emacs.d/lisp/"
-if [ ! -d "$emacsddir" ]
-then
-    echo ------------------------------------------------------------
-    echo "$emacsddir doesn't exists. It will be created."
-    ProgressBar ${step} ${_end}; step=$((step+1))
-    mkdir -v -p $HOME/.emacs.d/lisp/
-    echo; echo
-else
-    echo ------------------------------------------------------------
-    echo "$emacsddir exists."
-    ProgressBar ${step} ${_end}; step=$((step+1))
-    echo; echo
-fi
+            sudo mv emacs-24.5.desktop /usr/share/applications/
+            ;;
+        * )
+            echo "Skipped."; echo; echo
+            ;;
+    esac
+}
 
 #----------------------------------------------------------------------
+# Move emacs init files and extentions.
 
-dotemacs="$HOME/.emacs"
-if [ -f "$dotemacs" ]
-then
-    echo ------------------------------------------------------------
-    echo "~/.emacs file found."
-    echo "Do you want replace it? [ y ]"
-    ProgressBar ${step} ${_end}; step=$((step+1))
-    read opcao
-    case $opcao in
-        y )
-            cd $HOME/GitLab/emacs/
-            cp -v dotemacs.el ~/.emacs
-            cp -v functions.el ~/.emacs.d/lisp/
-            cp -v prelude-packages.el ~/.emacs.d/lisp/
-            echo; echo
-            ;;
-        * )
-            echo "Skipped."; echo; echo
-            ;;
-    esac 
-else
-    echo ------------------------------------------------------------
-    echo "~/.emacs file not found."
-    echo "It will be created."
-    ProgressBar ${step} ${_end}; step=$((step+1))
-    cd $HOME/GitLab/emacs/
-    cp -v dotemacs.el ~/.emacs
-    cp -v functions.el ~/.emacs.d/lisp/
-    cp -v prelude-packages.el ~/.emacs.d/lisp/
-    echo; echo
-fi
+function moveemacsfiles {
+    
+    emacsddir="$HOME/.emacs.d/lisp/"
+    if [ ! -d "$emacsddir" ]
+    then
+        echo ------------------------------------------------------------
+        echo "$emacsddir doesn't exists. It will be created."
+        mkdir -v -p $HOME/.emacs.d/lisp/
+        echo; echo
+    else
+        echo ------------------------------------------------------------
+        echo "$emacsddir exists."
+        echo; echo
+    fi
+    
+    dotemacs="$HOME/.emacs"
+    if [ -f "$dotemacs" ]
+    then
+        echo ------------------------------------------------------------
+        echo "~/.emacs file found."
+        echo "Do you want replace it? [y]es/[q]uit"
+        read opcao
+        case $opcao in
+            y )
+                cd $HOME/GitLab/emacs/
+                cp -v dotemacs.el ~/.emacs
+                cp -v functions.el ~/.emacs.d/lisp/
+                cp -v prelude-packages.el ~/.emacs.d/lisp/
+                echo; echo
+                ;;
+            * )
+                echo "Skipped."; echo; echo
+                ;;
+        esac 
+    else
+        echo ------------------------------------------------------------
+        echo "~/.emacs file not found."
+        echo "It will be created."
+        cd $HOME/GitLab/emacs/
+        cp -v dotemacs.el ~/.emacs
+        cp -v functions.el ~/.emacs.d/lisp/
+        cp -v prelude-packages.el ~/.emacs.d/lisp/
+        echo; echo
+    fi
+}
 
-##----------------------------------------------------------------------
-## Send line or region for shell buffer. 
+#----------------------------------------------------------------------
+# Send line or region for shell buffer. 
 
-file="$HOME/.emacs.d/lisp/essh.el"
-if [ -f "$file" ]
-then
-    echo ------------------------------------------------------------
-    echo "~/.emacs.d/lisp/essh.el file found."
-    echo "Do you want update it? [ y ]"
-    ProgressBar ${step} ${_end}; step=$((step+1))
-    read opcao
-    case $opcao in
-        y )
-            cd $HOME/.emacs.d/lisp/
-            wget -N 'http://www.emacswiki.org/emacs/download/essh.el'
-            ;;
-        * )
-            echo "Skipped."; echo; echo
-            ;;
-    esac 
-else
-    echo ------------------------------------------------------------
-    echo "~/.emacs.d/lisp/essh.el file not found."
-    echo "It will be created."
-    ProgressBar ${step} ${_end}; step=$((step+1))
-    cd $HOME/.emacs.d/lisp/
-    wget -N 'http://www.emacswiki.org/emacs/download/essh.el'
-    echo; echo
-fi
+function downloadessh {
+    file="$HOME/.emacs.d/lisp/essh.el"
+    if [ -f "$file" ]
+    then
+        echo ------------------------------------------------------------
+        echo "~/.emacs.d/lisp/essh.el file found."
+        echo "Do you want update it? [y]es/[q]uit"
+        read opcao
+        case $opcao in
+            y )
+                cd $HOME/.emacs.d/lisp/
+                wget -N 'http://www.emacswiki.org/emacs/download/essh.el'
+                ;;
+            * )
+                echo "Skipped."; echo; echo
+                ;;
+        esac 
+    else
+        echo ------------------------------------------------------------
+        echo "~/.emacs.d/lisp/essh.el file not found."
+        echo "It will be created."
+        cd $HOME/.emacs.d/lisp/
+        wget -N 'http://www.emacswiki.org/emacs/download/essh.el'
+        echo; echo
+    fi
+}
 
-##----------------------------------------------------------------------
+#----------------------------------------------------------------------
+# Cicle among options.
 
-if which tree >/dev/null
-then
-    echo ------------------------------------------------------------
-    echo "This is the last step of the process."
-    echo "Do you want show the tree of ~/.emacs.d/ ? [ y ]"
-    ProgressBar ${step} ${_end}; step=$((step+1))
-    read opcao
-    case $opcao in
-        y )
-            tree -L 2 ~/.emacs.d/
-            ;;
-        * )
-            echo "Skipped."; echo; echo
-            ;;
-    esac 
-fi
+while :
+do
+    printf "\nMenu of options\n\n"
+    printf "  1. Install Emacs-24.5.\n"
+    printf "  2. Make desktop application for Emacs-24.5.\n"
+    printf "  3. Move .emacs and .emacs.d/.\n"
+    printf "  4. Download and move essh.el.\n"
+    printf "  q. Quit.\n\n"
+    
+    read -sn1 -p "Select (1,2,3,4,q): " input
+    echo
 
-ProgressBar ${step} ${_end}; step=$((step+1))
+    case $input in
+        1) installemacs24.5 ;;
+        2) makedesktopapp ;;
+        3) moveemacsfiles ;;
+        4) downloadessh ;;
+        q) break ;;
+        *) echo "Invalid seletion" ;;
+    esac
+done
+
+#----------------------------------------------------------------------
+# End of session.
+
 cat << EOF
+
 --------------------------------------------------------------------------
 
   You have finished the installation of Emacs. Congratulations!
