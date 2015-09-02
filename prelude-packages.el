5c0176adf0780d3401b8e68c5e6d4e84a24dac38
Walmes Zeviani <walmeszeviani@gmail.com>
2015-09-02 12:16:04 -0300

    Include yasnippet and r-autoyas.

--- prelude-packages.el
+++ prelude-packages.el
@@ -26,8 +26,10 @@
     markdown-toc
     polymode
     popup
+    r-autoyas
     with-editor
-    yafolding)
+    yafolding,
+    yasnippet)
   "A list of packages to ensure are installed at launch.")
 
 (defun prelude-packages-installed-p ()
