6c8c098f1083e7646f11aec220f748449b60d778
Walmes Zeviani <walmes@ufpr.br>
2016-01-23 16:26:43 -0200

    Add helm and company packages.

--- dotemacs.el
+++ dotemacs.el
@@ -145,6 +145,85 @@
 ;; Extensions.
 ;;----------------------------------------------------------------------
 
+;;----------------------------------------------------------------------
+
+;; (package-initialize)
+;; (if (require 'quelpa nil t)
+;;     (quelpa-self-upgrade)
+;;   (with-temp-buffer
+;;     (url-insert-file-contents
+;;      "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
+;;     (eval-buffer)))
+
+(setq quelpa-upgrade-p nil)
+
+(quelpa 'helm :upgrade nil)
+(quelpa 'company :upgrade nil)
+
+;;----------------------------------------------------------------------
+;; helm.
+;; http://tuhdo.github.io/helm-intro.html
+
+(require 'helm)
+(require 'helm-config)
+
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-z") 'helm-select-action)
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t
+      helm-move-to-line-cycle-in-source     t
+      helm-ff-search-library-in-sexp        t
+      helm-scroll-amount                    8
+      helm-ff-file-name-history-use-recentf t
+      helm-M-x-fuzzy-match                  t
+      helm-buffers-fuzzy-matching           t
+      helm-recentf-fuzzy-match              t
+      helm-locate-fuzzy-match               t
+      helm-apropos-fuzzy-match              t
+      helm-lisp-fuzzy-completion            t
+      helm-semantic-fuzzy-match             t
+      helm-imenu-fuzzy-match                t)
+
+(when (executable-find "ack-grep")
+  (setq helm-grep-default-command
+        "ack-grep -Hn --no-group --no-color %e %p %f"
+        helm-grep-default-recurse-command
+        "ack-grep -H --no-group --no-color %e %p %f"))
+
+(helm-mode 1)
+(helm-autoresize-mode t)
+
+(global-set-key (kbd "M-x")     'helm-M-x)
+(global-set-key (kbd "M-y")     'helm-show-kill-ring)
+(global-set-key (kbd "C-x b")   'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+
+(add-to-list 'helm-sources-using-default-as-input
+             'helm-source-man-pages)
+
+;;----------------------------------------------------------------------
+;; company.
+
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(setq company-idle-delay 0.2
+      company-minimum-prefix-length 2
+      company-require-match nil
+      company-dabbrev-ignore-case nil
+      company-dabbrev-downcase nil
+      company-frontends '(company-pseudo-tooltip-frontend))
+
+;;----------------------------------------------------------------------
+
 ;; To work the accents on Sony Vaio.
 (require 'iso-transl)
 
