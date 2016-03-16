83a63c7cdbb6072b217060d1464e4998b921c14d
Walmes Zeviani <walmes@ufpr.br>
2016-03-16 14:33:49 -0300

    Add helm and company list of packages.

--- library-install.el
+++ library-install.el
@@ -1,13 +1,13 @@
 ;;======================================================================
 ;; This file makes installation of emacs packages in the melpa
 ;; repository. You must run it in Linux terminal by:
-;; 
+;;
 ;; $ emacs --batch -l library-install.el
 ;;
 ;; All the packages listed bellow will be installed. The commented
 ;; packages are dependencies from the non commented ones. ATTENTION! It
 ;; assumes Emacs >=24.4, althought was testet only with Emacs 24.5.2.
-;; 
+;;
 ;; Inspired by:
 ;; http://hacks-galore.org/aleix/blog/archives/2013/01/08/install-emacs-packages-from-command-line
 ;; http://www.emacswiki.org/emacs/BatchMode
@@ -20,21 +20,25 @@
 
 (package-initialize)
 
+(list-packages)
+
+(package-install 'helm)
 (package-install 'auctex)
 (package-install 'auto-complete)
 (package-install 'bm)
 (package-install 'bookmark+)
+(package-install 'company)
 (package-install 'color-theme)
 (package-install 'color-theme-solarized)
 (package-install 'ess)
 (package-install 'ess-R-data-view)
 (package-install 'ess-R-object-popup)
-(package-install 'leuven-theme)
+;; (package-install 'leuven-theme)
 (package-install 'magit)
-(package-install 'magit-popup)
 (package-install 'markdown-mode)
 (package-install 'markdown-toc)
 (package-install 'polymode)
+(package-install 'smartparens)
 (package-install 'yafolding)
 ;(package-install 'async)
 ;(package-install 'ctable)
