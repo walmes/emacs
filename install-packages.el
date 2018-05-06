7d56387913b447953053bf64e215d592ce73d630
Walmes Zeviani <walmes@ufpr.br>
2018-05-06 17:28:09 -0300

    Update to install packages in batch mode.

--- install-packages.el
+++ install-packages.el
@@ -1,19 +1,18 @@
 ;; ATTENTION: Run it in terminal in batch mode.
-;;   emacs -batch -l install-packages.el
-;; UPDATE: it does not work as expected.  Is is better run each package-install.
+;;   emacs --script install-packages.el
 
 (require 'package)
+(package-initialize)
 
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;;(add-to-list 'package-archives
+;;             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+;;(add-to-list 'package-archives
+;;             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
-(package-initialize)
 (package-list-packages)
 
 ;; To install a package.
@@ -49,14 +48,14 @@
 (package-install 'company)
 (package-install 'bm)
 (package-install 'bookmark)
-;; (package-install 'color-theme)
-;; (package-install 'color-theme-solarized)
 (package-install 'monokai-theme)
 (package-install 'smartparens)
 (package-install 'yafolding)
 (package-install 'ess)
 (package-install 'ess-R-data-view)
 (package-install 'elpy)
+(package-install 'jedi)
+(package-install 'jedi-core)
 (package-install 'magit)
 (package-install 'markdown-mode)
 (package-install 'markdown-toc)
